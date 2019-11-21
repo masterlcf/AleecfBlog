c247e4086011e7f952756ccc222e6c6342df2996
lcf <leecf1014@163.com>
2019-11-21 16:31:17 +0800

    rm home

--- vue_blog/src/App.vue
+++ vue_blog/src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    111
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
